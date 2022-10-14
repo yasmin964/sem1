275c0da82097d961d86bd864c3e8eb12d3b6263e
yasya <kalgexavva1970@icloud.com>
2022-10-14 23:05:26 +0300

    new text

--- example/Program.cs
+++ example/Program.cs
@@ -1,5 +1,20 @@
-﻿Console.WriteLine("введите первое число: ");
+﻿//задача 2= принимает 2 числа,определяет какое большее и меньшее
+Console.WriteLine("введите первое число: ");
 int num1=int.Parse(Console.ReadLine()!);
 Console.WriteLine("введите второе число: ");
 int num2=int.Parse(Console.ReadLine()!);
-if 
+if (num1>num2)
+{
+  
+    System.Console.WriteLine($"максимальное число {num1}");
+}
+else if(num1<num2)
+{
+
+    System.Console.WriteLine($"максимальное число {num2}");
+}
+else
+{
+    System.Console.WriteLine($"максимального числа нет, первое=второе");
+
+}
