16bf63b1f521d986228c05f26be2731799af0187
yasya <kalgexavva1970@icloud.com>
2022-10-14 23:28:08 +0300

    some info

--- example2/Program.cs
+++ example2/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿// программа принимает на вход 3 числа и выдает максимальное
+Console.WriteLine("введите первое число: ");
+int num1=int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите второе число: ");
+int num2=int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите третье число: ");
+int num3=int.Parse(Console.ReadLine()!);
+if (num1>num2)and(num1>num3);
+Console.WriteLine($"Максимальное число {num1}");
+else if(num2>num3)and(num2>num1);
+Console.WriteLine($"максимальное число {num2}");
+
+else if(num3>num2)and(num3>num1);
+Console.WriteLine($"максимальное число {num3}");
+
+
+else
+
+    Console.WriteLine($"максимального числа нет, первое=второе"); 
+
+
