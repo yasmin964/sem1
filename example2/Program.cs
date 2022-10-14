558dc1e6ddbb14421a4615e0bd09cc7f375c7686
yasya <kalgexavva1970@icloud.com>
2022-10-14 23:37:32 +0300

    some info

--- example2/Program.cs
+++ example2/Program.cs
@@ -1,22 +1,22 @@
 ﻿// программа принимает на вход 3 числа и выдает максимальное
-Console.WriteLine("введите перове число:");
-int num1=int.Parse(Console.ReadLine()!);
-Console.WriteLine("введите второе число: ");
-int num2=int.Parse(Console.ReadLine()!);
-Console.WriteLine("введите третье число: ");
-int num3=int.Parse(Console.ReadLine()!);
-if (num1>num2)and(num1>num3)
-    Console.WriteLine ($"максимальное число {num1});
-else if(num2>num3)and(num2>num1);
-    Console.WriteLine($"максимальное число {num2}");
+// Console.WriteLine("введите перове число:");
+// int num1=int.Parse(Console.ReadLine()!);
+// Console.WriteLine("введите второе число: ");
+// int num2=int.Parse(Console.ReadLine()!);
+// Console.WriteLine("введите третье число: ");
+// int num3=int.Parse(Console.ReadLine()!);
+// if (num1>num2)and(num1>num3)
+//     Console.WriteLine ($"максимальное число {num1});
+// else if(num2>num3)and(num2>num1);
+//     Console.WriteLine($"максимальное число {num2}");
 
-else if(num3>num2)and(num3>num1);
-Console.WriteLine($"максимальное число {num3}");
+// else if(num3>num2)and(num3>num1);
+// Console.WriteLine($"максимальное число {num3}");
 
+// else
 
-else
+//     Console.WriteLine($"максимального числа нет, первое=второе");
 
-    Console.WriteLine($"максимального числа нет, первое=второе");
 
 
 
